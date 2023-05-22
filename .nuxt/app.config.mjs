ac8378e50c059a8f5917082ecf0463f21b2487f5
self-managed <self-managed@users.noreply.github.com>
2023-05-22 10:15:28 +0800

    Modify: modify start command, remove .nuxt folder

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/stu/web_2023/web0521/render/TutorGurus_Frontend/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
