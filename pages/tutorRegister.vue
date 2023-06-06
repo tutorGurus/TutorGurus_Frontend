de854af2be27eaf16862935d13f8060af5223990
Derek Tsuei <alberto040213@gmail.com>
2023-06-06 22:56:30 +0800

    feat: change baseURL

--- pages/tutorRegister.vue
+++ pages/tutorRegister.vue
@@ -146,8 +146,8 @@ const register = () => {
   };
   $fetch("/tutors/v1/register", {
     method: "POST",
-    baseURL: "http://localhost:8000",
-    // baseURL: "https://tutorgurus-backend.onrender.com",
+    // baseURL: "http://localhost:8000",
+    baseURL: "https://tutorgurus-backend.onrender.com",
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -169,8 +169,8 @@ const getUserData = () => {
   const { value: token } = useCookie("token");
 
   $fetch(`/user/v1/profile`, {
-    baseURL: "http://localhost:8000",
-    // baseURL: "https://tutorgurus-backend.onrender.com",
+    // baseURL: "http://localhost:8000",
+    baseURL: "https://tutorgurus-backend.onrender.com",
     headers: {
       Authorization: `Bearer ${token}`,
     },
