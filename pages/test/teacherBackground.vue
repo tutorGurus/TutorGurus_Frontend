dbe122857f8ab0168825a464a1d7e8afd303c6ed
self-managed <self-managed@users.noreply.github.com>
2023-06-11 18:58:53 +0800

    Add: Move in-progress pages

---
+++ pages/test/teacherBackground.vue
@@ -0,0 +1,128 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+
+const introduction = ref('')
+const title = ref('')
+const teaching_category = ref([])
+const educational_background = ref('')
+const work_experience = ref([])
+const notice = ref('')
+const teaching_introduction = ref([])
+
+function textChange() {
+  console.log('text Change')
+}
+
+function getUserData() {
+  console.log('get Dta')
+  introduction.value = '<p>多年教學經驗，讓我開始思考，為什麼孩子對於數學的認知及學習程度會有落差？</p><p>要如何幫助孩子們不排斥數學、喜歡上數學、甚至愛上數學呢？讓Kiki老師來告訴你！'
+  title.value = '王牌家教老師'
+  teaching_category.value = ['數學']
+  educational_background.value = "Background text"
+  work_experience.value = 'Work experience'
+  notice.value = '用Zoom上課，請提早上線等候！'
+  teaching_introduction.value = "數的運算、分數的運算、一元一"
+}
+
+function saveContent() {
+  fetch('/api/content', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: title.value,
+      teaching_category: teaching_category.value,
+      introduction: introduction.value,
+      educational_background: educational_background.value,
+      work_experience: work_experience.value,
+      notice: notice.value,
+      teaching_introduction: teaching_introduction.value
+    })
+  })
+}
+
+onMounted(() => {
+  const userData = sessionStorage.getItem('user')
+  console.log(userData)
+  if (userData === null) {
+    // alert("請先登入會員");
+    // .router.push("/");
+  } else {
+    getUserData()
+  }
+
+  getUserData()
+
+})
+</script>
+
+<template>
+  <div>
+    <UserInfo />
+    <div class="py-16">
+
+      <v-container>
+        <div>
+          <v-col cols="12">
+
+            <v-card class="mb-8 mx-auto pa-5 rounded-xl">
+              <h3>講師介紹</h3>
+
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field v-model="title" label="專屬頭銜" variant="outlined" />
+                </v-col>
+
+                <v-col cols="12">
+                  <v-select v-model="teaching_category" :items="['數學', '英文']" label="Select" multiple variant="outlined" />
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </div>
+      </v-container>
+
+      <div class="mb-8 mx-auto pa-5 rounded-xl quillbox">
+        <h3>簡單自介</h3>
+        <ClientOnly>
+          <QuillEditor v-model:content="introduction" theme="snow" content-type="html" @textChange="textChange" />
+        </ClientOnly>
+        <!-- <p> {{ introduction }}</p> -->
+      </div>
+
+      <div class="mb-8 mx-auto pa-5 rounded-xl quillbox">
+        <h3>教學背景</h3>
+        <ClientOnly>
+          <QuillEditor v-model:content="educational_background" theme="snow" content-type="html" @textChange="textChange" />
+        </ClientOnly>
+        <!-- <p> {{ educational_background }}</p> -->
+      </div>
+
+
+      <div class=" mb-8 mx-auto pa-5 rounded-xl quillbox">
+        <h3>課程簡介</h3>
+        <ClientOnly>
+          <QuillEditor v-model:content="teaching_introduction" theme="snow" content-type="html" @textChange="textChange" />
+        </ClientOnly>
+        <!-- <p> {{ teaching_introduction }}</p> -->
+      </div>
+
+      <div class="mb-8 mx-auto pa-5 rounded-xl quillbox">
+        <h3>上課注意事項</h3>
+        <ClientOnly>
+          <QuillEditor v-model:content="notice" theme="snow" content-type="html" @textChange="textChange" />
+        </ClientOnly>
+        <!-- <p> {{ notice }}</p> -->
+      </div>
+
+      <v-btn color="success" @click="saveContent">Save</v-btn>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
